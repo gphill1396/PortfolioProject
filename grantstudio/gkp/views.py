5dcc1620dd4cfd31796aad7d278b47fb084fd048
gphill1396 <50145275+gphill1396@users.noreply.github.com>
2022-04-20 14:43:34 -0400

    tutorial part 3 12:24

--- grantstudio/gkp/views.py
+++ grantstudio/gkp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Home, About, Profile, Category, Skills, Portfolio
 
-# Create your views here.
+def index(request):
+
+    return render(request, 'index.html')
